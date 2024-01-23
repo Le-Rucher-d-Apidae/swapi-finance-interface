e68d342be26ec0f9e10d671e35cd6153b525ab52
BenoistP <bp_github.com@gazouille.net>
2024-01-23 20:58:00 +0100

    Update TotalSupply.ts

--- src/data/TotalSupply.ts
+++ src/data/TotalSupply.ts
@@ -7,8 +7,7 @@ import { useSingleCallResult } from '../state/multicall/hooks'
 // or contract total supply cannot be fetched
 export function useTotalSupply(token?: Token): TokenAmount | undefined {
   const contract = useTokenContract(token?.address, false)
-
   const totalSupply: BigNumber = useSingleCallResult(contract, 'totalSupply')?.result?.[0]
-
+  // console.debug('useTotalSupply totalSupply=', totalSupply)
   return token && totalSupply ? new TokenAmount(token, totalSupply.toString()) : undefined
 }
