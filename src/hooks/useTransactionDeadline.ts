fe8cc5e8757de615258276343b0f3291133de1fa
BenoistP <bp_github.com@gazouille.net>
2024-01-24 10:44:04 +0100

    useTransactionDeadline
    comments

--- src/hooks/useTransactionDeadline.ts
+++ src/hooks/useTransactionDeadline.ts
@@ -4,8 +4,23 @@ import { AppState } from '../state'
 
 // combines the current timestamp with the user setting to give the deadline that should be used for any submitted transaction
 export default function useTransactionDeadline(): BigNumber | undefined {
+  // debugger
+  /*
   const ttl = useSelector<AppState, number>(state => state.user.userDeadline)
-  const currentTimestamp = BigNumber.from(new Date().getTime() + 100000)
-  if (currentTimestamp && ttl) return currentTimestamp.add(ttl)
+  const currentTimestamp = BigNumber.from(new Date().getTime() + 100_000)
+  // if (currentTimestamp && ttl) return currentTimestamp.add(ttl)
+  if (currentTimestamp && ttl) return currentTimestamp.add(ttl * 1_000)
   return undefined
+*/
+  let txDeadline = undefined
+  const ttl = useSelector<AppState, number>(state => state.user.userDeadline)
+  const currentTimestamp = BigNumber.from(new Date().getTime() + 100_000) // minimum deadline is 100 seconds from now
+  // if (currentTimestamp && ttl) return currentTimestamp.add(ttl)
+  if (currentTimestamp && ttl) txDeadline = currentTimestamp.add(ttl * 1_000) // ms to s
+  // console.debug(`useTransactionDeadline ttl=${ttl} currentTimestamp=${currentTimestamp} txDeadline=${txDeadline}`)
+  // if (txDeadline) {
+  //   const dt = new Date(txDeadline.toNumber())
+  //   console.debug(`useTransactionDeadline dt=${dt}`)
+  // }
+  return txDeadline
 }
