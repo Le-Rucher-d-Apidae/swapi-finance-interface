4d429273f56a7caba86ed83892731cba4cb0fed6
BenoistP <bp_github.com@gazouille.net>
2024-01-25 18:46:33 +0100

    useColor: get default color from theme

--- src/hooks/useColor.ts
+++ src/hooks/useColor.ts
@@ -1,4 +1,4 @@
-import { useState, useLayoutEffect } from 'react'
+import { useState, useLayoutEffect, useContext } from 'react'
 import { shade } from 'polished'
 import Vibrant from 'node-vibrant'
 import { hex } from 'wcag-contrast'
@@ -10,6 +10,7 @@ import {
   TOKEN_LIST_ASSET_GENERIC_ADDRESS_DEFAULT_LOGO_URL_BASE,
   TOKEN_LIST_ASSET_GENERIC_ADDRESS_DEFAULT_LOGO_URL_DEFAULT_LOGO
 } from '../constants/index'
+import { ThemeContext } from 'styled-components'
 
 async function getColorFromToken(token: Token): Promise<string | null> {
   // const path = token.equals(BAG[token.chainId]) ?
@@ -23,7 +24,7 @@ async function getColorFromToken(token: Token): Promise<string | null> {
       TOKEN_LIST_EXCHANGE_CUSTOM_ASSET_DEFAULT_LOGO_URL
     : `${process.env.REACT_APP_TOKEN_LIST_ASSET_GENERIC_ADDRESS_LOGO_URL_BASE}${token.address}${process.env.REACT_APP_TOKEN_LIST_ASSET_GENERIC_ADDRESS_LOGO_URL_DEFAULT_LOGO}` ||
       `${TOKEN_LIST_ASSET_GENERIC_ADDRESS_DEFAULT_LOGO_URL_BASE}${token.address}${TOKEN_LIST_ASSET_GENERIC_ADDRESS_DEFAULT_LOGO_URL_DEFAULT_LOGO}`
-  // console.debug('path', path)
+  console.debug('swapi-finance-interface/src/hooks/useColor.ts path=', path)
   return Vibrant.from(path)
     .getPalette()
     .then(palette => {
@@ -42,13 +43,18 @@ async function getColorFromToken(token: Token): Promise<string | null> {
 }
 
 export function useColor(token?: Token) {
-  const [color, setColor] = useState('#2172E5')
+  const theme = useContext(ThemeContext)
+  // const defaultColor = '#2172E5'
+  const defaultColor = theme.primary1
+  // const [color, setColor] = useState('#2172E5')
+  const [color, setColor] = useState(defaultColor)
 
   useLayoutEffect(() => {
     let stale = false
 
     if (token) {
       getColorFromToken(token).then(tokenColor => {
+        console.debug('swapi-finance-interface/src/hooks/useColor.ts tokenColor=', tokenColor)
         if (!stale && tokenColor !== null) {
           setColor(tokenColor)
         }
@@ -57,7 +63,7 @@ export function useColor(token?: Token) {
 
     return () => {
       stale = true
-      setColor('#2172E5')
+      setColor(defaultColor)
     }
   }, [token])
 
