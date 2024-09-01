4ce4d8593aab4fcef78213eb4565a562d0d3dda2
BenoistP <bp_github.com@gazouille.net>
2024-09-01 10:49:04 +0200

    clean

--- src/components/Header/TokenBalanceContent.tsx
+++ src/components/Header/TokenBalanceContent.tsx
@@ -1,6 +1,5 @@
 import { ChainId, TokenAmount, CURRENCY, WCURRENCY, JSBI } from '@swapi-finance/sdk'
-import React, { /* useState, */ useContext } from 'react'
-// import { useEffect } from 'react'
+import React, { useContext } from 'react'
 import { X } from 'react-feather'
 import styled, { ThemeContext } from 'styled-components'
 import tokenLogo from '../../assets/images/token-logo.png'
@@ -19,12 +18,6 @@ import { Color } from '../../theme/styled'
 const ContentWrapper = styled(AutoColumn)`
   width: 100%;
 `
-
-// const ModalUpper = styled(DataCard)`
-//   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-//   background: radial-gradient(76.02% 75.41% at 1.84% 0%, #e8c177 0%, #ecd0a3 100%);
-//   padding: 0.5rem;
-// `
 const ModalUpper = styled(DataCard)`
   border-radius: 12px;
   border: 1px solid ${({ theme }) => theme.accountModalBorder};
@@ -53,94 +46,18 @@ export default function TokenBalanceContent({ setShowTokenBalanceModal }: { setS
   const theme = useContext(ThemeContext)
   const selfToken = chainId ? SELF_TOKEN[chainId] : SELF_TOKEN[ChainId.POLYGON]
 
-  // const [circulatingSupply, setCirculatingSupply] = useState<TokenAmount>()
-  // const [circulatingSupply /* , setCirculatingSupply */] = useState<TokenAmount>()
-  // const [circulatingSupply /* , setCirculatingSupply */] = useState<TokenAmount>(defaultTokenAmount)
-  // const bag = chainId ? BAG[chainId] : undefined
-  // const apd = chainId ? SELF_TOKEN[chainId] : SELF_TOKEN[ChainId.POLYGON]
-  // const defaultTokenAmount = new TokenAmount(apd, JSBI.BigInt(0))
-  // console.log('TokenBalanceContent apd=', apd)
-  // console.dir(apd)
   const total = useAggregateSelfTokenBalance()
-  /* 
-  const total = selfToken
-    ? new TokenAmount(selfToken, JSBI.BigInt(0))
-    : new TokenAmount(SELF_TOKEN[ChainId.POLYGON], JSBI.BigInt(0)) // useAggregateSelfTokenBalance()
- */
-  // console.log('TokenBalanceContent total=', total)
-  // console.dir(total)
-  const selfTokenBalance: TokenAmount | undefined = useTokenBalance(account ?? undefined, /* bag */ selfToken)
+  const selfTokenBalance: TokenAmount | undefined = useTokenBalance(account ?? undefined, selfToken)
   const treasuryAccoutTokenBalance: TokenAmount | undefined = useTokenBalance(
     `${process.env.REACT_APP_TREASURY_ACCOUNT}` ?? undefined,
     selfToken
   )
 
-  const totalSupply: TokenAmount | undefined = useTotalSupply(/* bag */ selfToken)
-  // const totalSupply_: TokenAmount | undefined = useTotalSupply(/* bag */ apd)
-  // console.log('TokenBalanceContent totalSupply_=', totalSupply_)
-  // const totalSupply = useMemo(() => {
-  //   if (apd) {
-  //     if (totalSupply_) {
-  //       return totalSupply_
-  //     }
-  //     // return new TokenAmount(apd, JSBI.BigInt(0))
-  //   }
-  //   return totalSupply_
-  // }, [apd])
+  const totalSupply: TokenAmount | undefined = useTotalSupply(selfToken)
 
-  // useEffect(() => {
-  // 	bag && chainId === ChainId.AVALANCHE
-  // 		? computeSelfTokenCirculation(bag)
-  // 			.then(circulating => {
-  // 				setCirculatingSupply(circulating)
-  // 			})
-  // 		: setCirculatingSupply(totalSupply)
-  // }, [bag, chainId, totalSupply])
-  /*
-  useEffect(() => {
-    if (apd && chainId === ChainId.POLYGON) {
-      computeSelfTokenCirculation(apd).then(circulating => {
-        // setCirculatingSupply(circulating)
-        console.log('TODO: IF setCirculatingSupply circulating=', circulating)
-      })
-    } else {
-      console.log('TODO: ELSE setCirculatingSupply totalSupply=', totalSupply)
-      // setCirculatingSupply(undefined)
-      setCirculatingSupply(new TokenAmount(SELF_TOKEN[ChainId.POLYGON], JSBI.BigInt(0)))
-      // totalSupply && setCirculatingSupply(totalSupply)
-    }
-  }, [apd, chainId, totalSupply])
-*/
-  /* 
-  useEffect(() => {
-    if (selfToken && chainId === ChainId.POLYGON) {
-      computeSelfTokenCirculation(selfToken).then(circulating => {
-        setCirculatingSupply(circulating)
-        // console.log('TODO: IF setCirculatingSupply circulating=', circulating)
-      })
-    } else {
-      // console.log('TODO: ELSE setCirculatingSupply totalSupply=', totalSupply)
-      // setCirculatingSupply(undefined)
-      // setCirculatingSupply(new TokenAmount(SELF_TOKEN[ChainId.POLYGON], JSBI.BigInt(0)))
-      // totalSupply && setCirculatingSupply(totalSupply)
-      // avoid loop: set CirculatingSupply if not already set
-      !circulatingSupply && totalSupply && setCirculatingSupply(totalSupply)
-      // console.debug(`apd=${chainId}, chainId=${chainId}, totalSupply=${totalSupply}`)
-    }
-  }, [selfToken, chainId, totalSupply, circulatingSupply])
- */
-
-  // TODO: Determine our token (SELF_TOKEN) price in WCURRENCY
-  // TODO: Determine our token (SELF_TOKEN) price in WCURRENCY
-  // TODO: Determine our token (SELF_TOKEN) price in WCURRENCY
-  // TODO: Determine our token (SELF_TOKEN) price in WCURRENCY
-  // TODO: Determine our token (SELF_TOKEN) price in WCURRENCY
-  // TODO: Determine our token (SELF_TOKEN) price in WCURRENCY
-  // Determine BAG price in AVAX
-  // const wavax = WAVAX[chainId ? chainId : 43114]
   const wcurrency = WCURRENCY[CHAINID]
 
-  const [currencySelfTokenTokenPairState, currencySelfTokenTokenPair] = usePair(wcurrency, /* bag */ selfToken)
+  const [currencySelfTokenTokenPairState, currencySelfTokenTokenPair] = usePair(wcurrency, selfToken)
   let tokenPriceInCurrency: number | undefined
   if (currencySelfTokenTokenPairState && currencySelfTokenTokenPair && selfToken) {
     const currencySelfTokenRatio = JSBI.divide(
@@ -151,11 +68,9 @@ export default function TokenBalanceContent({ setShowTokenBalanceModal }: { setS
   }
 
   const usdToken = USDC[CHAINID] // USD Token in use ; WCurrency / USD token must exist to be able to calculate price !
-  // console.log('TokenBalanceContent usdToken=', usdToken)
   const [currencyUSDTokenPairState, currencyUSDTokenPair] = usePair(wcurrency, usdToken)
 
   let tokenPriceInUSD: number | undefined
-  // const [, currencySelfTokenTokenPair] = usePair(wcurrency, /* bag */ selfToken)
   if (currencyUSDTokenPairState && currencyUSDTokenPair && selfToken) {
     const currencyUSDTokenRatio = JSBI.divide(
       JSBI.multiply(oneToken18JSBI, currencyUSDTokenPair.reserveOf(usdToken).raw),
